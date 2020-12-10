ccdd04fee2916182890a0af6d1aee15927cec323
EScharnick <eas97227@bethel.edu>
2020-12-10 16:57:37 -0600

    corrected error in pdf calculation

--- dumpMuPDFfactors.py
+++ dumpMuPDFfactors.py
@@ -24,17 +24,31 @@ dirlist = [
 
 from ROOT import TFile, TH1
 
+year = '2018'
+if year == '2018':
+    dirlist.append('BprimeBprime_M-900_TuneCP5_PSweights_13TeV-madgraph-pythia8')
+    dirlist.append('TprimeTprime_M-900_TuneCP5_PSweights_13TeV-madgraph-pythia8')
+
 for sample in dirlist:
+    if year == '2018': sample = sample.replace('CP5_13TeV','CP5_PSweights_13TeV')
     print('---------------------'+sample+'--------------------------')
-    runlist = EOSlistdir('/store/user/jmanagan/FWLJMET102X_1lep2018mupdf_072919/'+sample+'/singleLep2017/')
+    runlist = EOSlistdir('/store/user/lpcljm/FWLJMET102X_MuRF'+year+'_Sep2020/'+sample+'/singleLep'+year+'/')
     if len(runlist) > 1: 
         print('PROBLEM: more than 1 crab directory, SKIPPING')
         continue
-
-    rfile = TFile.Open('root://cmseos.fnal.gov//store/user/jmanagan/FWLJMET102X_1lep2018mupdf_072919/'+sample+'/singleLep2017/'+str(runlist[0])+'/0000/'+sample+'_1.root')
-    hist = rfile.Get("mcweightanalyzer/weightHist")
-    integral = hist.GetBinContent(5) + hist.GetBinContent(7)
-    newpdf = hist.GetBinContent(2)
+    integral = 0.0
+    newpdf = 0.0
+    if sample == 'BprimeBprime_M-1000_TuneCP5_PSweights_13TeV-madgraph-pythia8' and year == '2018':
+	for i in range(1,14):		# loop to deal with BB1000 getting split into 13 files in 2018
+	    rfile = TFile.Open('root://cmseos.fnal.gov//store/user/lpcljm/FWLJMET102X_MuRF'+year+'_Sep2020/'+sample+'/singleLep'+year+'/'+str(runlist[0])+'/0000/'+sample+'_'+str(i)+'.root')
+	    hist = rfile.Get("mcweightanalyzer/weightHist")
+	    integral = integral + hist.GetBinContent(5) + hist.GetBinContent(7)
+	    newpdf = newpdf + hist.GetBinContent(3)
+    else:
+	rfile = TFile.Open('root://cmseos.fnal.gov//store/user/lpcljm/FWLJMET102X_MuRF'+year+'_Sep2020/'+sample+'/singleLep'+year+'/'+str(runlist[0])+'/0000/'+sample+'_1.root')
+	hist = rfile.Get("mcweightanalyzer/weightHist")
+	integral = hist.GetBinContent(5) + hist.GetBinContent(7)
+	newpdf = hist.GetBinContent(3)
 
     print(str(round(newpdf,3))+'. # from integral '+str(integral))
 
@@ -48,20 +62,27 @@ for sample in dirlist:
     print('MuRF up yield = '+str(round(muvars[6],3)))
     print('MuRF dn yield = '+str(round(muvars[0],3)))
     
-    pdfhist = rfile.Get("mcweightanalyzer/pdfcounts")
+    pdfhist = rfile.Get("mcweightanalyzer/pdf4LHCcounts")
     #print('PDF nominal yield = '+str(round(pdfhist.GetBinContent(1),3)))
 
     pdfvars = []
     for ibin in range(1,pdfhist.GetNbinsX()+1): pdfvars.append(pdfhist.GetBinContent(ibin))
+    pdfnominal = pdfvars[0]
     pdfvars.sort()
 
-    print('PDF up yield = '+str(round(pdfvars[83],3)))
-    print('PDF dn yield = '+str(round(pdfvars[15],3)))
+    errsqr = 0
+    for ibin in range(1,pdfhist.GetNbinsX()+1): errsqr = errsqr + (pdfvars[ibin-1]-pdfnominal)*(pdfvars[ibin-1]-pdfnominal)
+    err = pow(errsqr,0.5)
+
+    print('error/pdfnominal = '+str(round(err/pdfnominal,3)))
+    print('pdfnominal/error = '+str(round(pdfnominal/err,3)))
+    print('(pdfnominal + error)/pdfnominal = '+str(round((pdfnominal + err)/pdfnominal,3)))
+    print('pdfnominal/(pdfnominal + error) = '+str(round(pdfnominal/(pdfnominal + err),3)))
+    print('(pdfnominal - error)/pdfnominal = '+str(round((pdfnominal - err)/pdfnominal,3)))
+    print('pdfnominal/(pdfnominal - error) = '+str(round(pdfnominal/(pdfnominal - err),3)))
 
     print('MUup scale factor = '+str(round(muhist.GetBinContent(1)/muvars[6],3)))
     print('MUdn scale factor = '+str(round(muhist.GetBinContent(1)/muvars[0],3)))
-    print('PDFup scale factor = '+str(round(pdfhist.GetBinContent(1)/pdfvars[83],3)))
-    print('PDFdn scale factor = '+str(round(pdfhist.GetBinContent(1)/pdfvars[15],3)))
 
 
 
